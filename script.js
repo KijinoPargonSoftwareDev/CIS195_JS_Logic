83df57a0c238d12800e34ab56a4849801f76b047
KijinoParagon <115103246+KijinoParagon@users.noreply.github.com>
2026-08-14 15:21:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Movie Explorer
+// Your job is to make the webpage interactive!
+
+// 1. Create your movies array here.
+
+
+
+// 2. Display the movies on the webpage.
+
+
+
+// 3. Add the required buttons and interactions.
+
+
+
+// 4. Demonstrate the required JavaScript concepts.
